0df10294d7f9183a0fb1c9e4cfc212cb7f89683c
Thu Nguyen <ngthu1995@gmail.com>
2019-03-06 17:49:56 -0500

    finish contact

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -4,16 +4,7 @@ describe("MenuController", () => {
   beforeEach(() => {
     this.menu = new MenuController();
   });
-  describe("#getContactCount()", () => {
-    it("should return 0 when no contacts are in the book", () => {
-      expect(this.menu.getContactCount()).toBe(0);
-    });
 
-    it("should return 1 when there is exactly 1 contact are in the book", () => {
-      this.menu.book.addContact("Bob", "555-555-5555");
-      expect(this.menu.getContactCount()).toBe(1);
-    });
-  });
   describe("#remindMe()", () => {
     it("should return 'Learning is a life-long pursuit' if reminder works", () => {
       expect(this.menu.remindMe());
